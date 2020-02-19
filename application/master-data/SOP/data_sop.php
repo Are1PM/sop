efb4f12249881bd8bb69e3da3549ff7d220ee44e
root <root@localhost.localdomain>
2020-02-20 00:47:53 +0800

    ke-3 20 february 2020

--- application/master-data/SOP/data_sop.php
+++ application/master-data/SOP/data_sop.php
@@ -1,11 +1,10 @@
 <?php
+include 'koneksi.php';
+
 if (isset($_POST['go-data'])) {
   $kode = $_POST['kode_prodi'];
-
-  // query menampilkan data SOP jurusan
-
-  #codn query disini
-
+  $query = "SELECT * FROM tb_sop s, tb_jurusan j WHERE s.kode_jurusan=j.kode_jurusan AND s.kode_jurusan='$kode'";
+  $hasil = mysqli_query($conn, $query);
 } else {
   header('Location:?go=form-pilih-sop');
 }
@@ -32,7 +31,33 @@ if (isset($_POST['go-data'])) {
         </tr>
       </thead>
       <tbody>
+        <?php
+        $no = 1;
+        while ($data = mysqli_fetch_assoc($hasil)) :
+        ?>
+          <tr>
+            <td><?= $no ?></td>
+            <td><?= $data['judul'] ?></td>
+            <td><?= $data['isi_sop'] ?></td>
+            <td><?= $data['jurusan'] ?></td>
+            <td><?= $data['versi'] ?></td>
+            <td><?= $data['tgl_upload'] ?></td>
+            <td class="text-center">
+              <a class="btn btn-sm btn-primary" href="?go=ubah-jurusan&kd=<?= $data['kode_jurusan'] ?>">
+                <i class="fa fa-edit"></i> Edit
+              </a>
+              |
+              <a class="btn btn-sm btn-danger tombol-hapus" href="?go=hapus-jurusan&kd=<?= $data['kode_jurusan'] ?>" data-data="Jurusan <?= ucwords(strtolower($data['jurusan'])) ?>">
+                <i class="fa fa-trash"></i> hapus
+              </a>
+            </td>
+          </tr>
+        <?php
+          $no++;
+        endwhile;
+        ?>
       </tbody>
+
       <tfoot>
       </tfoot>
     </table>
--- application/master-data/SOP/form_pilih_sop.php
+++ application/master-data/SOP/form_pilih_sop.php
@@ -1,22 +1,29 @@
+<?php
+include 'koneksi.php';
+$query = mysqli_query($conn, 'SELECT * FROM tb_jurusan');
+?>
 <div class="row">
 	<div class="col-md-12">
-	  <div class="box box-info">
-	    <div class="box-header">
-		    <form method="post" action="index.php?go=data-sop">
-		      <div class="form-group">
-		        <label>JURUSAN/Prodi</label>
-		        <select class="form-control select2" name="kode_prodi" style="width: 100%;">
-		          <option selected="selected">--pilih--</option>
-		          <option value="F1A1">Matematika</option>
-		          <option value="F1A2">Statistik</option>
-		          <option value="F1A3">Ilmu Komputer</option>
-		        </select>
-		      </div> 
-		      <div class="box-footer">
-		          <button type="submit" name="go-data" class="btn btn-primary">Go</button>
-		      </div>
-		    </form>
+		<div class="box box-info">
+			<div class="box-header">
+				<form method="post" action="index.php?go=data-sop">
+					<div class="form-group">
+						<label>JURUSAN/Prodi</label>
+						<select class="form-control select2" name="kode_prodi" style="width: 100%;">
+							<option selected="selected">--pilih--</option>
+							<?php
+							while ($data = mysqli_fetch_array($query)) {
+								echo "<option value=" . $data['kode_jurusan'] . ">" . $data['jurusan'] . "</option>";
+							}
+
+							?>
+						</select>
+					</div>
+					<div class="box-footer">
+						<button type="submit" name="go-data" class="btn btn-primary">Go</button>
+					</div>
+				</form>
+			</div>
 		</div>
-	   </div>
 	</div>
 </div>
